e2f137dcba576e272416c5d55e2d6cf0a41dcd9f
balajiommudali1 <balaji.ommudali@gmail.com>
2017-08-23 06:28:48 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,12 +1,10 @@
+from pyvirtualdisplay import Display
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+browser = webdriver.Firefox()
+browser.get('http://www.python.org')
+
+browser.close()
